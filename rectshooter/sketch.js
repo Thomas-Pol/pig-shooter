c07c914a1e7c961f7475bc4dd797102121ab0aca
Thomas-Pol <thepollies.thomas@gmail.com>
2023-09-22 11:33:55 +0200

    final update pig shooter

--- rectshooter/sketch.js
+++ rectshooter/sketch.js
@@ -3,12 +3,23 @@ let kogelY = [];
 let vlesX = [];
 let vlesY = 220;
 let kogelCount = 0;
-let achterGround;
+let achterGrond;
 let varken;
+let hitMarker;
+let fade;
+let fadeAmount = 1;
+let varkenGeluid;
+let tacticalNuke;
+let varkenNuke;
+let varkenKillCounter = 0;
 
 function preload() {
-achterGround = loadImage("./pictures/pixel-art-arcade-game-scene-with-house-mill-trees-fence-e-clouds-8-bit-background-vector.jpg");
+achterGrond = loadImage("./pictures/pixel-art-arcade-game-scene-with-house-mill-trees-fence-e-clouds-8-bit-background-vector.jpg");
 varken = loadImage("./pictures/Naamloos-1.png");
+hitMarker = loadImage("./pictures/Naamloos-2.png");
+varkenGeluid = loadSound("./pictures/pig.mp3");
+tacticalNuke = loadSound("./pictures/tactical-nuke.mp3");
+varkenNuke = loadImage("./pictures/pigNuke.gif");
 }
 
 function setup() {
@@ -21,18 +32,24 @@ for(let i = 0; i < 5; i++) {
 
 function draw() {
   background(220);
-  image(achterGround, 0 , 0);
-  achterGround.resize(800,400);
+  image(achterGrond, 0 , 0);
+  achterGrond.resize(800,400);
   noFill();
   ellipse(mouseX, mouseY, 33, 33);
   for(let kogelCounter = 0; kogelCounter <= kogelCount; kogelCounter++) {
-    fill('red');
-    ellipse(kogelX[kogelCounter], kogelY[kogelCounter], 15, 15);
+    image(hitMarker, kogelX[kogelCounter] - 50, kogelY[kogelCounter] - 50, 100, 100); 
   }
   for(let i = 0; i < 5; i++) {
-    image(varken, vlesX[i], vlesY, 60, 100);
+    image(varken, vlesX[i], vlesY, 70, 100);
   }
+  fill('pink');
+  textSize(20);
+  text("varken-kills:" + varkenKillCounter, 10, 20);
 
+  if(varkenKillCounter == 100) {
+    image(varkenNuke, 0, 0);
+    varkenNuke.resize(800,400);
+  }
 }
 
 function mouseClicked() { 
@@ -40,9 +57,13 @@ function mouseClicked() {
   kogelY[kogelCount] = mouseY;
   kogelCount += 1;
   for(let i = 0; i < 5; i++) {
-  if(mouseX > vlesX[i] && mouseX < (vlesX[i] + 60) && mouseY > vlesY && mouseY < (vlesY + 100)) {
-    console.log("raak");
+  if(mouseX > vlesX[i] && mouseX < (vlesX[i] + 70) && mouseY > vlesY && mouseY < (vlesY + 100)) {
+    varkenGeluid.play();
     vlesX[i] = random(10, 740);
+    varkenKillCounter++;
+    if(varkenKillCounter == 100) {
+      tacticalNuke.play();
+    }
   }
   }
 }
